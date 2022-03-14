e5b839fdbf3744bc7ba861e93febc6efe9435dce
Artem <Luzifer_Morningstar@mail.ru>
2022-03-14 12:09:33 +0300

    Made NavBar component

---
+++ lib/components/navbar.dart
@@ -0,0 +1,53 @@
+import 'package:flutter/material.dart';
+
+class NavBar extends StatefulWidget{
+    const NavBar({Key? key}) : super(key: key);
+
+    @override
+    State<NavBar> createState() => _NavBarState();
+}
+
+class _NavBarState extends State<NavBar>{
+  int _bottomNavBarIndex = 0;
+
+  @override
+  Widget build(BuildContext context){
+    return BottomNavigationBar(
+      
+        type: BottomNavigationBarType.fixed,
+        currentIndex: _bottomNavBarIndex,
+        onTap: (int index) {
+          setState(() {
+            _bottomNavBarIndex = index;
+          });
+        },
+        items: const [
+          BottomNavigationBarItem(
+            icon: Icon(Icons.feed_outlined),
+            label: "Новости",
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.paid_outlined),
+            label: "Курсы валют",  
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.home),
+            label: "Главная",
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.settings),
+            label: "Настройки",
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.account_circle_outlined),
+            label: "Профиль",
+          ),
+        ],
+        showSelectedLabels: false,
+        showUnselectedLabels: false,
+        unselectedItemColor: const Color.fromARGB(255, 141, 147, 171),
+        selectedItemColor: Colors.white,
+        backgroundColor: const Color.fromARGB(255, 34, 34, 54),
+      );
+  }
+}
